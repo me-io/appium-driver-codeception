5e142da0cc02b3918d224b41d3eacaec151932c3
mohsinaheer1 <mohsin.aheer@tajawal.com>
2018-04-06 01:07:17 +0400

    add constant getclipboard

--- src/Traits/BaseConstants.php
+++ src/Traits/BaseConstants.php
@@ -546,4 +546,7 @@ class BaseConstants
 
     /** @var string */
     public static $SETCLIPBOARD = '/appium/device/set_clipboard';
+
+    /** @var string */
+    public static $GETCLIPBOARD = '/appium/device/get_clipboard';
 }
